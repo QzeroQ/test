2967eaf4fe4fbdd378d0022ad64c4498969f6f35
zhangbosong <wepush@sina.com>
2017-01-13 11:42:15 +0800

    初始

--- php1.php
+++ php1.php
@@ -1,10 +1,5 @@
-这是想用来测试重置操作
-<p style="margin-bottom:28px;text-align: center;">
-<span style="border: 1px solid #fff;padding: 10px 20px;color: #fff;">
-<i style="display:inline-block;font-family: initial;">距离开始还剩</i>
-<i style="padding:0 10px;">01</i>
-:<i style="padding:0 10px;">00</i>
-:<i style="padding:0 10px;">25</i>
-:<i style="padding:0 0 0 10px;">23</i>
-</span>
-</p>
+<?php 
+echo "1";
+
+
+ ?>
